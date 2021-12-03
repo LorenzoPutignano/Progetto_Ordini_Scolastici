a6f816a3053bb876f1386aca60c2192597777a2a
Lorenzo Putignano <92269232+LorenzoPutignano@users.noreply.github.com>
2021-12-03 23:54:08 +0100

    Add files via upload

---
+++ Ordinazione_panini2122/php/Products.php
@@ -0,0 +1,9 @@
+<?php
+require 'connDB.php';
+
+$sql_query = "select * from panini";
+$result = $conn->query($sql_query);
+foreach($result as $row){
+        echo $row["id"].";".$row["nome"].";".$row["prezzo"]."|";    
+    }
+?>
