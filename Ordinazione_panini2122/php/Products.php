c3d4831fab443da57175f216636740e46da089e0
Lorenzo Putignano <92269232+LorenzoPutignano@users.noreply.github.com>
2021-12-21 17:01:40 +0100

    Update Products.php

--- Ordinazione_panini2122/php/Products.php
+++ Ordinazione_panini2122/php/Products.php
@@ -1,6 +1,6 @@
 <?php
 require 'connDB.php';
-<!--bG9yeSBzaSDDqCBwb3Jjb2RpbyBhZmZlemlvbmF0byA6Jyk=-->
+<!--3e7a328631aef7801472104dbaf8f90d-->
 $sql_query = "select * from panini";
 $result = $conn->query($sql_query);
 foreach($result as $row){
